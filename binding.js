fc791470ca071ff5334718720443159aa5257494
Richard Screene <richard.screene@netdev.co.uk>
2014-09-15 14:51:20 +0100

    Add debug

--- binding.js
+++ binding.js
@@ -24,11 +24,13 @@ for (i = 0, count = vars.length; i < count; i++) {
 
 count = tryList.length;
 for (i = 0; i < count; i++) {
+console.log("file=", file);
     file = tryList[i];
     if (fs.existsSync(file)) {
         try {
             exports = require(file);
         } catch (e) {
+console.log("err=", e);
             continue; // try next
         }
         break;
